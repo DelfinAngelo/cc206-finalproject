60fedf9352327374f0f9da8395da0c16aa80d0ac
DelfinAngelo <delfinangelo.lamasan@wvsu.edu.ph>
2024-12-02 20:50:17 +0800

    Updated again

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 // File: lib/main.dart
 
 import 'package:flutter/material.dart';
+import 'completed_tasks_screen.dart';
 
-void main() => runApp(TaskMateApp());
+void main() => runApp(BuluhatonProApp());
 
-class TaskMateApp extends StatelessWidget {
+class BuluhatonProApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: TaskListScreen());
+    return MaterialApp(
+      title: 'Buluhaton Pro',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TaskListScreen(),
+    );
   }
 }
 
@@ -18,6 +25,14 @@ class TaskListScreen extends StatefulWidget {
 
 class _TaskListScreenState extends State<TaskListScreen> {
   final List<String> _tasks = [];
+  final List<String> _completedTasks = [];
+  final TextEditingController _taskController = TextEditingController();
+
+  @override
+  void dispose() {
+    _taskController.dispose();
+    super.dispose();
+  }
 
   void _addTask(String task) {
     setState(() {
@@ -25,17 +40,71 @@ class _TaskListScreenState extends State<TaskListScreen> {
     });
   }
 
-  void _removeTask(int index) {
+  void _markTaskCompleted(int index) {
     setState(() {
+      _completedTasks.add(_tasks[index]);
       _tasks.removeAt(index);
     });
+  }
+
+  void _navigateToCompletedTasks() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => CompletedTasksScreen(completedTasks: _completedTasks),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Buluhaton Pro')),
-      body: Center(child: Text('Value')),
+      appBar: AppBar(
+        title: Text('Buluhaton Pro - Task Manager'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.checklist),
+            onPressed: _navigateToCompletedTasks,
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _taskController,
+              decoration: InputDecoration(
+                labelText: 'Enter a task',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (_taskController.text.isNotEmpty) {
+                _addTask(_taskController.text);
+                _taskController.clear();
+              }
+            },
+            child: Text('Add Task'),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _tasks.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_tasks[index]),
+                  trailing: IconButton(
+                    icon: Icon(Icons.check, color: Colors.green),
+                    onPressed: () => _markTaskCompleted(index),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 
