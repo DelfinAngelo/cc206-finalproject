61adeabb0b2c1764e9f50c1edaf894319f914dbd
DelfinAngelo <delfinangelo.lamasan@wvsu.edu.ph>
2024-12-02 21:07:44 +0800

    Updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 // File: lib/main.dart
-
 import 'package:flutter/material.dart';
-import 'completed_tasks_screen.dart';
 
 void main() => runApp(BuluhatonProApp());
 
@@ -10,9 +8,6 @@ class BuluhatonProApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Buluhaton Pro',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: TaskListScreen(),
     );
   }
@@ -26,13 +21,6 @@ class TaskListScreen extends StatefulWidget {
 class _TaskListScreenState extends State<TaskListScreen> {
   final List<String> _tasks = [];
   final List<String> _completedTasks = [];
-  final TextEditingController _taskController = TextEditingController();
-
-  @override
-  void dispose() {
-    _taskController.dispose();
-    super.dispose();
-  }
 
   void _addTask(String task) {
     setState(() {
@@ -47,88 +35,11 @@ class _TaskListScreenState extends State<TaskListScreen> {
     });
   }
 
-  void _navigateToCompletedTasks() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => CompletedTasksScreen(completedTasks: _completedTasks),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Buluhaton Pro - Task Manager'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.checklist),
-            onPressed: _navigateToCompletedTasks,
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _taskController,
-              decoration: InputDecoration(
-                labelText: 'Enter a task',
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              if (_taskController.text.isNotEmpty) {
-                _addTask(_taskController.text);
-                _taskController.clear();
-              }
-            },
-            child: Text('Add Task'),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _tasks.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(_tasks[index]),
-                  trailing: IconButton(
-                    icon: Icon(Icons.check, color: Colors.green),
-                    onPressed: () => _markTaskCompleted(index),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
+      appBar: AppBar(title: Text('Buluhaton Pro - Your #1 Task Manager')),
+      body: Center(child: Text('Value')),
     );
   }
-
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(title: Text('Buluhaton Pro')),
-    body: Column(
-      children: [
-        Expanded(
-          child: ListView.builder(
-            itemCount: _tasks.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(_tasks[index]),
-              );
-            },
-          ),
-        ),
-      ],
-    ),
-  );
-
-
 }
-
-
