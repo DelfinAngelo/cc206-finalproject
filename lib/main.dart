80356b93af368db66a8e2d3c7cce7fa79504d76e
DelfinAngelo <delfinangelo.lamasan@wvsu.edu.ph>
2024-12-02 21:15:20 +0800

    Saved

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,7 @@
-<<<<<<< HEAD
-// File: lib/main.dart
-import 'package:flutter/material.dart';
-
-void main() => runApp(BuluhatonProApp());
-
-class BuluhatonProApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Buluhaton Pro',
-      home: TaskListScreen(),
-    );
-  }
-}
-
-class TaskListScreen extends StatefulWidget {
-  @override
-  _TaskListScreenState createState() => _TaskListScreenState();
-}
-
-class _TaskListScreenState extends State<TaskListScreen> {
-  final List<String> _tasks = [];
-  final List<String> _completedTasks = [];
-
-  void _addTask(String task) {
-    setState(() {
-      _tasks.add(task);
-    });
-  }
-
-  void _markTaskCompleted(int index) {
-    setState(() {
-      _completedTasks.add(_tasks[index]);
-      _tasks.removeAt(index);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Buluhaton Pro - Your #1 Task Manager')),
-      body: Center(child: Text('Value')),
-    );
-  }
-=======
 @override
 Widget build(BuildContext context) {
   return Scaffold(
-    appBar: AppBar(title: Text('Buluhaton Pro - Task Manager')),
+    appBar: AppBar(title: Text('Buluhaton Pro - Your #1 Task Manager')),
     body: Column(
       children: [
         Expanded(
@@ -63,5 +17,4 @@ Widget build(BuildContext context) {
       ],
     ),
   );
->>>>>>> layout-widget
 }
