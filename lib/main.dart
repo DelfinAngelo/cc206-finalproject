f76bb8cecc184b45666e19774b90b82e791b0618
DelfinAngelo <delfinangelo.lamasan@wvsu.edu.ph>
2024-12-02 18:27:03 +0800

    Saved changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,44 @@
 // File: lib/main.dart
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(TaskMateApp());
+
+class TaskMateApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: TaskListScreen());
+  }
+}
+
+class TaskListScreen extends StatefulWidget {
+  @override
+  _TaskListScreenState createState() => _TaskListScreenState();
+}
+
+class _TaskListScreenState extends State<TaskListScreen> {
+  final List<String> _tasks = [];
+
+  void _addTask(String task) {
+    setState(() {
+      _tasks.add(task);
+    });
+  }
+
+  void _removeTask(int index) {
+    setState(() {
+      _tasks.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Buluhaton Pro')),
+      body: Center(child: Text('Value')),
+    );
+  }
+
 @override
 Widget build(BuildContext context) {
   return Scaffold(
@@ -18,6 +58,8 @@ Widget build(BuildContext context) {
       ],
     ),
   );
+
+
 }
 
 
