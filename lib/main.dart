1211ee77fcf9608ecef0f08b99a55665f3c6367a
DelfinAngelo <delfinangelo.lamasan@wvsu.edu.ph>
2024-12-02 16:40:35 +0800

    Layout with Column and ListView Builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,21 @@
 // File: lib/main.dart
-import 'package:flutter/material.dart';
-
-void main() => runApp(TaskMateApp());
-
-class TaskMateApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: TaskListScreen());
-  }
-}
-
-class TaskListScreen extends StatefulWidget {
-  @override
-  _TaskListScreenState createState() => _TaskListScreenState();
-}
-
-class _TaskListScreenState extends State<TaskListScreen> {
-  final List<String> _tasks = [];
-
-  void _addTask(String task) {
-    setState(() {
-      _tasks.add(task);
-    });
-  }
-
-  void _removeTask(int index) {
-    setState(() {
-      _tasks.removeAt(index);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('TaskMate')),
-      body: Center(child: Text('Value Widgets Logic Placeholder')),
-    );
-  }
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(title: Text('Buluhaton Pro')),
+    body: Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: _tasks.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(_tasks[index]),
+              );
+            },
+          ),
+        ),
+      ],
+    ),
+  );
 }
